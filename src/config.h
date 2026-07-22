07202534aa0274298d6e70365a771d07288c2e7e
Sirakorn Lamyai <git@privaterelay.srakrn.me>
2026-07-23 00:47:26 +0700

    feat: bump version no

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 */
 
 
-const PROGMEM char* dk2mqtt_version = "1.3-b4";
+const PROGMEM char* dk2mqtt_version = "1.3a-srakrn";
 
 //Define global variables for files
 const PROGMEM char* wifi_conf = "/wifi.json";
